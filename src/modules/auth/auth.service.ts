1322351aa6cff1b7fc8b08a77678131ba50ec50b
Mohammad Mahabub Alam <mhmmdmahabubalam@gmail.com>
2026-05-01 19:25:25 +0600

    Refactor authentication service for login and verification
    Refactor login method to use LoginAuthDto and improve error handling. Added OTP generation for email verification.

--- src/modules/auth/auth.service.ts
+++ src/modules/auth/auth.service.ts
@@ -1,174 +1,174 @@
 import {
-  ForbiddenException,
+  BadRequestException,
   Injectable,
   InternalServerErrorException,
-  UnauthorizedException,
+  Logger,
 } from '@nestjs/common';
 import { JwtService } from '@nestjs/jwt';
-import { PrismaService } from 'src/prisma/prisma.service';
-import { LoginUserDto } from './dto/login.dto';
-import * as bcrypt from 'bcrypt';
+import { LoginAuthDto, LoginResponseDto } from './dto/login-auth.dto';
+import { UpdateAuthDto } from './dto/update-auth.dto';
+import { ConfigService } from '@nestjs/config';
+import { AuthRepository } from './auth.repository';
+import { SecurityService } from 'src/core/security/security.service';
 import { plainToInstance } from 'class-transformer';
-import { LoginResponseDto } from './dto/login-response.dto';
+import { MailService } from '../mail/mail.service';
+import { PrismaService } from 'src/prisma/prisma.service';
+import { sendResponse } from 'src/common/responses/send-response';
+
+type AccessTokenType = { id: number; email: string; role: string };
 
 @Injectable()
 export class AuthService {
+  private readonly logger = new Logger(AuthService.name);
   constructor(
     private readonly prisma: PrismaService,
+    private readonly authRepository: AuthRepository,
     private readonly jwtService: JwtService,
+    private readonly configService: ConfigService,
+    private readonly securityService: SecurityService,
+    private readonly mail: MailService,
   ) {}
 
-  // ------------- login
-  async login(loginUserDto: LoginUserDto) {
-    const user = await this.prisma.user.findUnique({
-      where: { email: loginUserDto.email },
-      select: {
-        id: true,
-        sid: true,
-        email: true,
-        username: true,
-        password_hash: true,
-        first_name: true,
-        last_name: true,
-        phone: true,
-        is_email_verified: true,
-        is_phone_verified: true,
-        is_two_factor_enabled: true,
-        ship_org_id: true,
-        user_group: true,
-        is_active: true,
-        last_login: true,
-        last_password_change: true,
-        created_at: true,
-        updated_at: true,
-        created_by: true,
-        status: true,
-        auth_version: true,
-        navy_id: true,
-      },
-    });
-    const financialYearDetails = await this.prisma.financialYear.findUnique({
-      where: {
-        id: Number(loginUserDto.financial_year),
-      },
-      select: {
-        id: true,
-        year_name: true,
-        is_active: true,
-      },
-    });
+  // ========== login ===========
+  async login(loginAuthDto: LoginAuthDto) {
+    try {
+      const { email, password } = loginAuthDto;
 
-    if (!user) {
-      throw new UnauthorizedException('Invalid Credentials');
-    }
-    if (!financialYearDetails) {
-      throw new UnauthorizedException('Invalid financial year.');
-    }
-    if (user.status !== 'ACTIVE') {
-      throw new ForbiddenException(
-        'Your account is currently inactive. Please contact support for assistance.',
+      const user = await this.authRepository.checkEmail(email);
+      this.logger.log('user data =>', user);
+      if (!user) {
+        throw new Error('Incorrect credentials.');
+      }
+
+      const isPasswordMatch = await this.securityService.comparePassword(
+        password,
+        user.passwordHash,
       );
+      if (!isPasswordMatch) {
+        throw new Error('Incorrect credentials.');
+      }
+
+      const payload = {
+        id: user.id,
+        email: user.email,
+        role: user.role,
+      };
+      const token = await this.generateToken(payload);
+
+      return plainToInstance(LoginResponseDto, {
+        id: user.id,
+        sid: user.sid,
+        name: user.name,
+        email: user.email,
+        role: user.role,
+        isActive: user.isActive,
+        emailVerified: user.emailVerified,
+        tokens: {
+          accessToken: token.accessToken,
+          refreshToken: token.refreshToken,
+        },
+      });
+    } catch (error: any) {
+      this.logger.error('login error =>', error);
+      throw new BadRequestException(error.message);
     }
-    if (!financialYearDetails.is_active) {
-      throw new ForbiddenException('Financial year is not active.');
-    }
-
-    const isPassValid = await bcrypt.compare(
-      loginUserDto.password,
-      user.password_hash,
-    );
-    if (!isPassValid) {
-      throw new UnauthorizedException('Invalid credential');
-    }
-
-    const token = await this.generateTokens(
-      String(user.id),
-      user.email,
-      user.user_group,
-      {
-        id: financialYearDetails.id,
-        year_name: financialYearDetails.year_name,
-      },
-      '',
-      user.auth_version,
-    );
-
-    //TODO ==========  logging here
-
-    return plainToInstance(LoginResponseDto, {
-      email: user.email,
-      username: user.username,
-      role: user.user_group,
-      accessToken: token.accessToken,
-      refreshToken: token.refreshToken,
-    });
   }
 
-  // generate tokens
-  private async generateTokens(
-    userId: string,
-    email: string,
-    role: string,
-    financial_year: {
-      id: string | number;
-      year_name: string;
-    },
-    scope?: string,
-    auth_version?: bigint,
-  ) {
+  // ========== send verification email
+  async sendVerificationEmailOtp(userId: number) {
+    try {
+      const user = await this.prisma.user.findUniqueOrThrow({
+        where: { id: userId },
+      });
+
+      if (user.emailVerified) {
+        throw new Error('Invalid request.');
+      }
+
+      const otp = this.generateOtp();
+      const expiresAt = new Date(Date.now() + 3 * 60 * 1000);
+
+      await this.prisma.user.update({
+        where: { id: userId },
+        data: {
+          verifyOtp: otp,
+          verifyOtpExpiry: expiresAt,
+        },
+      });
+
+      await this.mail.sendEmailVerificationOtp(user.email, otp);
+      return { message: 'Email Verification otp sent successfully.' };
+    } catch (error: any) {
+      this.logger.error('send email otp error =>', error);
+      throw new BadRequestException(error.message);
+    }
+  }
+
+  // ========= verify email =========
+  async verifyEmail(userId: number, otp: string) {
+    try {
+      const user = await this.prisma.user.findUniqueOrThrow({
+        where: { id: userId },
+      });
+
+      if (!user.verifyOtpExpiry || !user.verifyOtp) {
+        throw new Error('OTP verification failed');
+      }
+
+      if (user.verifyOtpExpiry < new Date() || user.verifyOtp !== otp) {
+        throw new Error('Invalid or expired OTP');
+      }
+
+      await this.prisma.user.update({
+        where: { id: userId },
+        data: {
+          emailVerified: true,
+          verifyOtp: null,
+          verifyOtpExpiry: null,
+        },
+      });
+
+      return { message: 'Email verified successfully' };
+    } catch (error: any) {
+      this.logger.error('email verification error =>', error);
+      throw new BadRequestException(error.message);
+    }
+  }
+
+  private generateOtp(): string {
+    return Math.floor(100000 + Math.random() * 900000).toString();
+  }
+
+  async generateToken(data: AccessTokenType) {
     try {
       const payload = {
-        sub: userId,
-        email: email,
-        role: role,
-        financial_year: financial_year,
-        client_id: process.env.CLIENT_ID,
-        scope: scope || '',
-        auth_version: auth_version,
+        sub: data.id,
+        email: data.email,
+        role: data.role,
+        type: 'access',
       };
-
       const refreshTokenPayload = {
-        sub: userId,
-        email: email,
-        financial_year: financial_year,
+        sub: data.id,
+        email: data.email,
+        role: data.role,
+        type: 'refresh',
       };
 
       const [accessToken, refreshToken] = await Promise.all([
-        this.jwtService.signAsync(payload, { expiresIn: '1d' }),
-        this.jwtService.signAsync(refreshTokenPayload, { expiresIn: '7d' }),
+        this.jwtService.signAsync(payload, {
+          expiresIn: '1d',
+          secret: this.configService.get<string>('JWT_ACCESS_SECRET'),
+        }),
+        this.jwtService.signAsync(refreshTokenPayload, {
+          expiresIn: '7d',
+          secret: this.configService.get<string>('JWT_REFRESH_SECRET'),
+        }),
       ]);
 
       return { accessToken, refreshToken };
     } catch (error) {
+      this.logger.error('Token generation failed', error);
       throw new InternalServerErrorException('Token Generation failed');
     }
   }
 }
-
-//  id: int
-//     sid: str
-//     email: EmailStr
-//     username: str
-//     first_name: str
-//     last_name: str
-//     phone: str | None
-//     is_email_verified: bool
-//     is_phone_verified: bool
-//     is_two_factor_enabled: bool
-//     ship_org_id: int | None
-
-//     ship_org_name: str | None = None
-//     role_id: int | None = None
-//     role_name: str | None = None
-
-//     user_group: UserGroupEnum
-//     is_active: bool
-//     last_login: datetime | None = None
-//     last_password_change: datetime | None = None
-//     created_at: datetime
-//     updated_at: datetime
-//     created_by: int | None = None
-//     status: StatusEnum
-//     auth_version: int
-//     navy_id: str | None = None
-//     assigned_role: int | None = None

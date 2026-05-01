87bcd8703106d2423b6e33c0aee823bdc104257e
Mohammad Mahabub Alam <mhmmdmahabubalam@gmail.com>
2026-05-01 19:25:47 +0600

    Refactor AuthController for login and email verification

--- src/modules/auth/auth.controller.ts
+++ src/modules/auth/auth.controller.ts
@@ -1,46 +1,95 @@
 import {
-  BadRequestException,
   Body,
   Controller,
+  HttpCode,
   HttpStatus,
   Post,
-  Res,
+  Req,
+  UseGuards,
 } from '@nestjs/common';
-import { ApiBody, ApiOperation, ApiResponse, ApiTags } from '@nestjs/swagger';
+import {
+  ApiBearerAuth,
+  ApiBody,
+  ApiOperation,
+  ApiResponse,
+} from '@nestjs/swagger';
+import { Request } from 'express';
+import { UserRole } from 'prisma/generated/enums';
 import { AuthService } from './auth.service';
-import { LoginResponseDto } from './dto/login-response.dto';
-import { LoginUserDto } from './dto/login.dto';
-import { Response } from 'express';
-import { sendResponse } from 'src/common/responses/send-response';
+import { Roles } from './decorator/roles.decorator';
+import { LoginAuthDto } from './dto/login-auth.dto';
+import { VerifyEmailDto } from './dto/verifyEmail.dto';
+import { JwtAuthGuard } from './guards/jwt-auth.guard';
+import { RolesGuard } from './guards/roles.guard';
 
-@ApiTags('Authentication')
 @Controller('auth')
 export class AuthController {
   constructor(private readonly authService: AuthService) {}
 
-  // ------------ login --------------
   @Post()
-  @ApiOperation({
-    summary: 'User login',
-    description: 'Authenticate user and return JWT tokens',
-  })
+  @HttpCode(HttpStatus.OK)
+  @ApiOperation({ summary: 'User Login' })
+  @ApiBody({ type: LoginAuthDto })
   @ApiResponse({
     status: 200,
-    description: 'Login Successful.',
-    type: LoginResponseDto,
+    description: 'Login successful.',
   })
-  @ApiBody({ type: LoginUserDto })
-  async login(@Body() loginDto: LoginUserDto, @Res() res: Response) {
-    try {
-      const result = await this.authService.login(loginDto);
-      sendResponse(res, {
-        statusCode: HttpStatus.OK,
-        success: true,
-        message: 'Login Successful.',
-        data: result,
-      });
-    } catch (error: any) {
-      throw new BadRequestException(error.message);
-    }
+  @ApiResponse({
+    status: HttpStatus.BAD_REQUEST,
+    description: 'Validation error.',
+  })
+  async login(@Body() loginAuthDto: LoginAuthDto) {
+    return await this.authService.login(loginAuthDto);
   }
+
+  // =========== Get email verification code
+  @Post('send-enail-otp')
+  @ApiBearerAuth('JWT-auth')
+  @UseGuards(JwtAuthGuard, RolesGuard)
+  @Roles(UserRole.admin_user, UserRole.normal_user)
+  @HttpCode(HttpStatus.OK)
+  @ApiOperation({ summary: 'Get email verification code' })
+  @ApiResponse({
+    status: 200,
+    description: 'OTP send successfully.',
+  })
+  @ApiResponse({
+    status: HttpStatus.BAD_REQUEST,
+    description: 'Invalid request',
+  })
+  async sendEmailVerificationOtp(@Req() req: Request) {
+    const userId = Number((req.user as { id: string }).id);
+    return await this.authService.sendVerificationEmailOtp(userId);
+  }
+
+  // =========== verify email otp
+  @Post('verify-email')
+  @ApiBearerAuth('JWT-auth')
+  @UseGuards(JwtAuthGuard, RolesGuard)
+  @Roles(UserRole.admin_user, UserRole.normal_user)
+  @HttpCode(HttpStatus.OK)
+  @ApiOperation({ summary: 'Verify email' })
+  @ApiBody({ type: VerifyEmailDto })
+  @ApiResponse({
+    status: 200,
+    description: 'Email verified successfully.',
+  })
+  @ApiResponse({
+    status: HttpStatus.BAD_REQUEST,
+    description: 'Validation error.',
+  })
+  async verifyEmail(@Req() req: Request, @Body() verifyEmail: VerifyEmailDto) {
+    const userId = Number((req.user as { id: string }).id);
+    return await this.authService.verifyEmail(userId, verifyEmail.otp);
+  }
+
+  // @Get(':id')
+  // findOne(@Param('id') id: string) {
+  //   return this.authService.findOne(+id);
+  // }
+
+  // @Patch(':id')
+  // update(@Param('id') id: string, @Body() updateAuthDto: UpdateAuthDto) {
+  //   return this.authService.update(+id, updateAuthDto);
+  // }
 }
